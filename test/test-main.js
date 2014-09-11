0381cd948998c9be6fa2785e7e8b98cf6a9db8f2
Qeole <qeole@outlook.com>
2014-09-11 22:45:13 +0200

    Unit tests (not verifying page content)

--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,130 @@
-var main = require("./main");
+/* Vim: set ts=8 sts=2 et sw=2 tw=80: */
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
+var self  = require("sdk/self");
+var tabs  = require("sdk/tabs");
+var utils = require('sdk/test/utils');
+var main  = require("./main");
+
+function clickInPanel(languageId) {
+  main.panel.languageId = languageId;
+  main.panel.hide();
+  main.doHighlight();
+}
+
+exports["test main01 startup"] = function(assert, done) {
+  assert.ok(!main.button.state("tab").checked, "button toggled off");
+  assert.ok(!main.panel.isShowing,             "panel is closed");
+  done();
 };
 
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
+exports["test main02 double click"] = function(assert, done) {
+  assert.ok(!main.button.state("tab").checked, "button toggled off");
+  assert.ok(!main.panel.isShowing,             "panel is closed");
+
+  main.button.click();
+  utils.waitUntil(function(){return main.panel.isShowing;}).then(() => {
+    assert.ok( main.button.state("tab").checked, "button toggled on");
+    assert.ok( main.panel.isShowing,             "panel is open");
+
+    main.button.click();
+    assert.ok(!main.button.state("tab").checked, "button toggled off");
+    /*
+     * Double click on button through JavaScript does not seem to close panel.
+     * Close it manually.
+     */
+    assert.ok( main.panel.isShowing,             "panel is open");
+    main.panel.hide();
+
+    utils.waitUntil(function(){return !main.panel.isShowing;}).then(() => {
+      assert.ok(!main.button.state("tab").checked, "button toggled off");
+      assert.ok(!main.panel.isShowing,             "panel is closed");
+      done();
+    });
+  });
+};
+
+exports["test main03 highlight empty"] = function(assert, done) {
+  assert.ok(!main.button.state("tab").checked, "button toggled off");
+  assert.ok(!main.panel.isShowing,             "panel is closed");
+
+  main.button.click();
+  utils.waitUntil(function(){return main.panel.isShowing;}).then(() => {
+    assert.ok( main.button.state("tab").checked, "button toggled on");
+    assert.ok( main.panel.isShowing,             "panel is open");
+
+    clickInPanel("auto");
+    main.doHighlight();
+    utils.waitUntil(function(){return !main.panel.isShowing;}).then(() => {
+      assert.ok(!main.button.state("tab").checked, "button toggled off");
+      assert.ok(!main.panel.isShowing,             "panel is closed");
+      done();
+    });
+  });
+};
+
+exports["test main04 highlight test page"] = function(assert, done) {
+  assert.ok(!main.button.state("tab").checked, "button toggled off");
+  assert.ok(!main.panel.isShowing,             "panel is closed");
+  tabs.open({
+    url     : self.data.url("test/random_c"),
+    onReady : () => {
+
+      main.button.click();
+      utils.waitUntil(function(){return main.panel.isShowing;}).then(() => {
+        assert.ok( main.button.state("tab").checked, "button toggled on");
+        assert.ok( main.panel.isShowing,             "panel is open");
+
+        clickInPanel("auto");
+        utils.waitUntil(function(){return  main.button.state("tab").checked &&
+                                          !main.panel.isShowing;}).then(() => {
+          assert.ok( main.button.state("tab").checked, "button toggled on");
+          assert.ok(!main.panel.isShowing,             "panel is closed");
+
+          main.button.click();
+          utils.waitUntil(function(){return !main.panel.isShowing;}).then(() => {
+            assert.ok(!main.button.state("tab").checked, "button toggled off");
+            assert.ok(!main.panel.isShowing,             "panel is closed");
+
+            tabs.activeTab.close();
+            done();
+          });
+        });
+      });
+    }
+  });
+};
+
+exports["test main05 highlight then reload"] = function(assert, done) {
+  assert.ok(!main.button.state("tab").checked, "button toggled off");
+  assert.ok(!main.panel.isShowing,             "panel is closed");
+  tabs.open({
+    url     : self.data.url("test/random_c"),
+    onReady : () => {
+
+      main.button.click();
+      utils.waitUntil(function(){return  main.button.state("tab").checked &&
+                                         main.panel.isShowing;}).then(() => {
+        assert.ok( main.button.state("tab").checked, "button toggled on");
+        assert.ok( main.panel.isShowing,             "panel is open");
+
+        clickInPanel("auto");
+        utils.waitUntil(function(){return  main.button.state("tab").checked &&
+                                          !main.panel.isShowing;}).then(() => {
+          assert.ok( main.button.state("tab").checked, "button toggled on");
+          assert.ok(!main.panel.isShowing,             "panel is closed");
+
+          tabs.activeTab.reload();
+          utils.waitUntil(function(){return !main.button.state("tab").checked;}).then(() => {
+            assert.ok(!main.button.state("tab").checked, "button toggled off");
+            assert.ok(!main.panel.isShowing,             "panel is closed");
+
+            tabs.activeTab.close();
+            done();
+          });
+        });
+      });
+    }
+  });
 };
 
 require("sdk/test").run(exports);
