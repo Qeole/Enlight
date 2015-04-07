7e4c324f39d038fac25650af286ceef497a2fc69
Qeole <qeole@outlook.com>
2015-04-07 15:34:57 +0200

    Highlight.js v8.5 -> Enlight v1.7

--- misc/extractLanguages.sh
+++ misc/extractLanguages.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sed 's/registerLanguage("/&\n/g' highlight.pack.js | sed 's/".*//' | sed '1d' | sort
+#sed 's/registerLanguage("/&\n/g' highlight.pack.js | sed 's/".*//' | sed '1d' | sort
+
+egrep -o 'registerLanguage\("[^"]+"' highlight.pack.js | sed 's/registerLanguage("\(.*\)"/\1/' | sort
