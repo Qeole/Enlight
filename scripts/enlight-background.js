Qeole/Enlight

commit 99821c9abdf9696040a77458cc6e874ea6bcbb5c

Switch to web extension framework
Update all the source code to have a working web extension, compatible
with Firefox 48+? (tested on version 57).

Background script has been completely rewritten, even though much of the
logics remain the same. Content script remains closer to its previous
version. Option handling must now be done by the extension (no more by
the SDK): add an option page. The popup for language selection did not
change much.

Since we changed a lot of things, this commit (or rather, the previous
one) also makes some cleanup in the files tracked by Git. Furthermore,
README.md and icons have been updated.

Of course, this new version of the add-on is no more compatible with the
jpm tool, or with the add-on SDK APIs.

Change version number to 2+. Since the new reference for version numbers
allows for more dot-separated values, the add-on will follow the
following scheme: A.B.X.Y, where
- A is the major version, bumped to 2,
- B is the minor version, incremented for each modification to the
  add-on internal code,
- X is the major version of embedded highlight.js,
- Y is the minor version of embedded highlight.js.
highlight.js also has a third (build) number, that will not appear in
the add-on version number.

Differences in behavior:
- The icon of the button no longer changes aspect when the button is
  clicked.
- Some options disappeared, as I suspect nobody used them: namely, the
  option for preventing background coloring, as well as the option for
  using alternate icon set (now handled directly in the manifest) have
  both been removed.

Known issue: the add-on keeps one global state to remember if the tab
has already been colored or not. Of course, in the case when several
tabs are to be highlighted, this is a problem. This is not so big an
issue, since in this case:
- If the tab should be colored but the state is not correct, the first
  click to the add-on button is without effect. Second click works as

scripts/enlight-background.js
@@ -1,269 +1,222 @@
-/* vim: set ts=8 sts=2 et sw=2 tw=80 cc=80: */
-
-var self     = require("sdk/self");
-var buttons  = require("sdk/ui/button/toggle");
-var tabs     = require("sdk/tabs");
-var panels   = require("sdk/panel");
-var spref    = require("sdk/simple-prefs");
-var xhr      = require("sdk/net/xhr");
-var _        = require("sdk/l10n").get; // Localization
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 
 /*
- * Paths (under data/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-var gHJSPath       = "highlightjs";      // directory for highlight.js
-var gHJSScript     = "highlight.js";     // highlight.js lib, under gHJSPath/
-var gContentScript = "enlightscript.js"; // Enlight content script
 
 /*
- * List of supported languages (for display in button label)
+ * Paths.
  */
-var gLanguagePath = "languages-all.json" // language list, under data/
+var gHLJSPath      = "highlightjs/highlight.pack.js";
+var gContentScript = "scripts/enlight-content.js";
+var gLanguagePath  = "popup/languages-list_all.json";
+
+/*
+ * List of supported languages (for display in button label).
+ */
 var gLanguageList = {};
 
 /*
- * Icon sets for toggle button
+ * Current page status: highlighted, or not.
  */
-var gIconInit;
-var gIconOff;
-var gIconOn;
-
-function setIcons () {
-  var invert = spref.prefs["invert"] ? ".invert" : "";
-  gIconInit = { // actually links towards ./lightbulb_off-xx(.invert)?.png
-    "16": "./lightbulb_init-16" + invert + ".png",
-    "32": "./lightbulb_init-32.png",
-    "64": "./lightbulb_init-64.png"
-  };
-  gIconOff = {
-    "16": "./lightbulb_off-16" + invert + ".png",
-    "32": "./lightbulb_off-32.png",
-    "64": "./lightbulb_off-64.png"
-  };
-  gIconOn = {
-    "16": "./lightbulb_on-16" + invert + ".png",
-    "32": "./lightbulb_on-32.png",
-    "64": "./lightbulb_on-64.png"
-  };
-}
-setIcons();
+var isHighlighted = false;
 
 /*
- * Update preferences
+ * Options, hardcoded for now.
  */
-spref.on("", onPrefChange);
-function onPrefChange(aPref) {
-  switch (aPref) {
-    case "invert":
-      setIcons();
-      button.icon = gIconInit;
-      break;
-    default:
-      break;
+var options = {
+  hlstyle: "solarized-dark.css",
+  autohl: false,
+  linenumbers: false,
+};
+
+/*
+ * Callback.
+ */
+function onError(error) {
+  console.error("[enlight]: Error:", error);
+}
+
+/*
+ * Reload one option from storage.
+ */
+function reloadOption(aId, aDefault) {
+  let gettingItem = browser.storage.local.get(aId);
+  return gettingItem.then((res) => {
+    if (res[aId] != undefined)
+      options[aId] = res[aId];
+  }, onError);
+}
+
+/*
+ * Update button tooltip label ("title").
+ */
+function updateTitle(aLanguageId) {
+  if (aLanguageId)
+    browser.browserAction.setTitle({
+      title: "Enlight [" + gLanguageList[aLanguageId] + "]"
+    });
+  else
+    browser.browserAction.setTitle({title: "Enlight"});
+}
+
+/*
+ * Actually trigger syntax highlighting by injecting content scripts.
+ */
+function doHighlight(aLanguageId) {
+  browser.tabs.executeScript({
+    file: gHLJSPath
+  }).then(() => {
+    browser.tabs.executeScript({
+      code:
+        "window.enlightContentScriptOptions = {" +
+        "  language: '" + aLanguageId + "'," +
+        "  lineNumbers: " + options.linenumbers +
+        "};"
+    }).then(() => {
+      browser.tabs.executeScript({
+        file: gContentScript
+      }).then(() => {
+        if (aLanguageId == "undo")
+          return browser.tabs.removeCSS({
+            file: "/highlightjs/styles/" + options.hlstyle
+          });
+        else
+          return browser.tabs.insertCSS({
+            file: "/highlightjs/styles/" + options.hlstyle
+          });
+      });
+    });
+  });
+}
+
+/*
+ * Listener. Communicate with popup.
+ */
+function popupListener(aMsg, aSender, aSendResponse) {
+  if (aMsg.languageId) {
+    console.debug("[enlight] Required language:", aMsg.languageId);
+    doHighlight(aMsg.languageId);
+    isHighlighted = true;
+    updateTitle(aMsg.languageId);
+  } else if (aMsg.shouldOpenPopup) {
+    aSendResponse({shouldOpen: !isHighlighted});
+    if (isHighlighted) {
+      doHighlight("undo");
+      isHighlighted = false;
+      updateTitle();
+    }
   }
 }
 
 /*
- * Toggle button
- */
-var button = buttons.ToggleButton({
-  id      : "Highlighter",
-  label   : _("button_label"),
-  icon    : gIconInit,
-  onClick : handleClick
-});
-
-/*
- * Language selection panel
- */
-function panelSelect() {
-  window.addEventListener("click", function(event) {
-    let t = event.target;
-    if (t.nodeName == "DIV") {
-      self.port.emit("click-lang", t.getAttribute("id"));
-    }
-  }, false);
-}
-
-var panel = panels.Panel({
-  contentURL    : self.data.url("panel.html"),
-  contentScript : "(" + panelSelect.toString() + ")()",
-  onHide        : function() {
-    if (panel.languageId == "" || panel.languageId == undefined) {
-      button.state("tab", {"icon" : gIconOff, "checked" : false,
-                   "label" : _("button_label")});
-    }
-  }
-});
-
-panel.port.on("click-lang", function(languageId) {
-  panel.languageId = languageId;
-  panel.hide();
-  doHighlight(languageId);
-});
-
-/*
- * Auto-highlight source code pages
- * condition: document.body has single <pre></pre> child node
+ * Injected script. Check page contents, for auto-highlighting.
  */
 function checkBody() {
-  if (document.body && document.body.childNodes.length == 1 &&
-      document.body.firstChild.nodeName == "PRE" &&
-      document.location.toString()
-        .slice(0,"view-source:".length) != "view-source:") {
-    self.port.emit("isCodeBlock");
-  }
-};
-(function setAutoHighlight() {
-  tabs.on("ready", function(aTab) {
-    if (spref.prefs["autohl"]) {
-      let worker = aTab.attach({
-        contentScript: "(" + checkBody.toString() + ")()",
-      });
-      worker.port.on("isCodeBlock", function() {
-        button.state("tab", {"icon" : gIconOn, "checked" : true});
-        doHighlight("", aTab);
-      });
-    }
-  });
-})();
+  let port = browser.runtime.connect({name:"checkBodyPort"});
+  if (
+    document.body &&
+    document.body.childNodes.length == 1 &&
+    document.body.firstChild.nodeName == "PRE" &&
+    document.location.toString()
+      .slice(0,"view-source:".length) != "view-source:"
+  )
+    port.postMessage({isCodeBlock: true});
+  else
+    port.postMessage({isCodeBlock: false});
+  port.disconnect();
+}
 
 /*
- * Callbacks
+ * Listen to injected script to see if it detects a code block in the page.
  */
-function handleClick(state) {
-  /*
-   * Classic behavior (window-wise) would be:
-   *    // checked is false: clicking a first time set it to true
-   *    if (checked)      // i.e. we just clicked an odd time
-   *      do highlight
-   *    else              // we just clicked an even time
-   *      undo highlight
-   *    fi
-   * But here we toggle tab-wise, so we need to check "manually".
-   *
-   * Also first time we click tab.checked is false, click does not set it to
-   * true. Hence we need a way to know if it's the first time for this tab:
-   * here we change the icon path (actually is a link to same PNG file).
-   */
-  button.state("window", {"checked" : false});
-  if (button.state("tab").icon["16"] == gIconInit["16"]) {
-    button.state("tab", {"icon" : gIconOn, "checked" : false});
-  }
-
-  if (!button.state("tab").checked) {
-    button.state("tab", {"icon" : gIconOn, "checked" : true});
-    panel.languageId = "";
-    panel.show({
-      position: button
-    });
-  }
-  else {
-    button.state("tab", {"icon" : gIconOff, "checked" : false,
-                 "label" : _("button_label")});
-    undoHighlight();
+function checkBodyListener(p) {
+  switch (p.name) {
+    case "checkBodyPort":
+      p.onMessage.addListener((m) => {
+        console.debug("[enlight] Do we detect a code block?", m.isCodeBlock);
+        doHighlight("auto");
+        isHighlighted = true;
+      });
+      break;
+    case "detectedLanguage":
+      p.onMessage.addListener((m) => {
+        console.debug("[enlight] Content script detected language:",
+          m.language);
+        updateTitle(m.language);
+      });
+      break;
+    default: {};
   }
 }
 
-function doHighlight(aLanguageId, aTab=tabs.activeTab) {
-  console.debug(_("log_highlight", aLanguageId, spref.prefs["style"]));
-
-  let worker = aTab.attach({
-    contentScriptOptions: {
-      "stylesheet"  : self.data.url(gHJSPath + "/styles/" +
-                      spref.prefs["style"] + ".css"),
-      "bgColor"     : spref.prefs["bgColor"],
-      "language"    : aLanguageId,
-      "lineNumbers" : spref.prefs["lineNumbers"]
-    },
-    contentScriptFile: [
-      self.data.url(gHJSPath + "/" + gHJSScript),
-      self.data.url(gContentScript)
-    ]
-  });
-
-  worker.port.on("toggle_off", function(reason) {
-    /*
-     * For some reason document content is not highlighted.
-     * Need to toggle button off.
-     */
-    if (!button.state("tab").checked) {
-      return;
-    }
-    let logToggleOffReason = _("log_toff_r" + reason);
-    console.debug(_("log_toggleoff", logToggleOffReason));
-    button.state("tab", {"icon" : gIconOff, "checked" : false,
-                 "label" : _("button_label")});
-  });
-
+/*
+ * Listener. If auto-highlighting is selected, highlight all raw text pages.
+ */
+function tabUpdateListener(aTabId, aChangeInfo, aTabInfo) {
+  if (!aChangeInfo.url)
+    return;
   /*
-   * Add language ID to button's label if we know it
+   * Auto-highlight!
    */
-  if (aLanguageId && aLanguageId != "auto") {
-    button.state("tab", {"icon" : gIconOn, "checked" : true,
-                 "label" : _("button_label") +
-                   " [" + gLanguageList[aLanguageId] + "]"});
-  }
-  else {
-    worker.port.on("detected_language", function(aId) {
-      /*
-       * We launched language auto-detection, and content script is telling us
-       * what language was detected: update button label
-       */
-      button.state("tab", {"icon" : gIconOn, "checked" : true,
-                   "label" : _("button_label") +
-                     " [" + gLanguageList[aId] + "]"});
-    });
-  }
-
-  aTab.on("ready", function () {
-    /*
-     * Content document has been changed/reloaded.
-     * Need to toggle button off.
-     */
-    if (!button.state("tab").checked) {
-      return;
-    }
-    console.debug(_("log_toggleoff", "log_toff_r2"));
-    button.state("tab", {"icon" : gIconOff, "checked" : false});
-    aTab.on("ready", function () {});
-  });
-}
-
-function undoHighlight() {
-  console.debug(_("log_undo"));
-  tabs.activeTab.attach({
-    contentScriptFile: [
-      self.data.url("enlightscript.js")
-    ]
+  browser.tabs.executeScript({
+    code: "(" + checkBody.toString() + ")()"
   });
 }
 
 /*
- * Functions used to parse JSON list of languages (so that we can display name
- * of detected language in button label)
+ * Parse JSON list of languages so that we can display the name of detected
+ * languages in button label.
  */
 function loadJSON(aCallback) {
-  var xobj = new xhr.XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open("GET", self.data.url(gLanguagePath), true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      aCallback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", gLanguagePath, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            aCallback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
 }
 
+/*
+ * Callback.
+ */
 function parseResponse(aResponse) {
   gLanguageList = JSON.parse(aResponse);
 }
 
-loadJSON(parseResponse);
+/*
+ * Load all options, and set up auto-highlighting if required.
+ */
+function init() {
+  if (options.autohl) {
+    browser.runtime.onConnect.removeListener(checkBodyListener);
+    browser.tabs.onUpdated.removeListener(tabUpdateListener);
+  }
+
+  reloadOption("autohl")
+    .then(reloadOption("linenumbers"))
+    .then(reloadOption("hlstyle"))
+    .then(() => {
+      if (options.autohl) {
+        browser.runtime.onConnect.addListener(checkBodyListener);
+        browser.tabs.onUpdated.addListener(tabUpdateListener);
+      }
+    });
+}
 
 /*
- * Exports for unit tests
+ * Perform these operations at add-on start-up:
  */
-exports.button      = button;
-exports.panel       = panel;
-exports.doHighlight = doHighlight;
+
+browser.runtime.onMessage.addListener(popupListener);
+
+browser.storage.onChanged.addListener(init);
+init();
+
+loadJSON(parseResponse);

icons/lightbulb-dark.svg
@@ -1,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="642.19"
+   height="642.18774"
+   id="svg2"
+   version="1.1"
+   inkscape:version="0.92.1 r15371"
+   sodipodi:docname="lightbulb-dark.svg"
+   inkscape:export-filename="/media/quentin/ESO/Firefox/addon-sdk/enlight/misc/lightbulb_off64.png"
+   inkscape:export-xdpi="8.9700003"
+   inkscape:export-ydpi="8.9700003">
+  <defs
+     id="defs4" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="0.65186683"
+     inkscape:cx="-108.63506"
+     inkscape:cy="330.10043"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     inkscape:window-width="1572"
+     inkscape:window-height="871"
+     inkscape:window-x="0"
+     inkscape:window-y="0"
+     inkscape:window-maximized="1"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0"
+     inkscape:snap-midpoints="true"
+     inkscape:snap-page="true"
+     inkscape:snap-object-midpoints="true"
+     inkscape:snap-bbox="true"
+     inkscape:snap-bbox-midpoints="true"
+     inkscape:snap-nodes="true"
+     inkscape:snap-grids="false"
+     inkscape:snap-to-guides="false" />
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-174.73873,-222.65557)">
+    <path
+       style="fill:#5a5b5f;fill-opacity:0.94117647;stroke:none"
+       d="M 321.72656 0 C 303.80784 0.00915 285.74653 2.3681119 269.5 7.0761719 C 190.42995 29.989772 135.98871 98.976526 133.38086 179.56445 C 132.54244 205.47337 136.00249 227.74479 144.67578 252.25781 C 148.30561 262.51666 166.36289 300.08837 175.83008 317.08203 C 186.91815 336.98516 203.61448 368.43654 206.80664 375.43359 C 212.29003 387.4529 215.83991 394.69153 217.03516 409.66016 C 218.18768 424.0936 220.64866 434.64095 224.76172 442.75781 C 229.11694 451.11906 234.76422 451.99773 240.47266 459.29102 C 298.83602 481.2596 348.91157 483.02592 403.13086 459.92773 C 405.37225 459.3416 416.3124 451.96567 420.48047 443.67773 C 424.09107 436.49827 425.8193 428.31675 427.08398 412.41797 C 428.48023 394.86504 429.57813 395.27461 432.70312 385.25391 C 435.7007 375.64184 450.75374 344.28352 462.33789 323.52148 C 481.39165 289.37179 495.11576 260.64398 500.11328 244.44922 C 515.99979 192.96801 510.28573 139.29074 484.05078 93.552734 C 459.5028 50.755794 422.3163 21.755828 373.0957 7.0234375 C 357.42062 2.3316775 339.64529 -0.00915 321.72656 0 z M 321.51562 81.134766 C 333.43645 81.128677 345.26126 82.687299 355.68945 85.808594 C 388.43453 95.609644 413.17478 114.90145 429.50586 143.37305 C 446.95924 173.80127 450.76025 209.51271 440.19141 243.76172 C 436.8667 254.53564 427.73649 273.6464 415.06055 296.36523 C 407.35395 310.17763 397.33795 331.03895 395.34375 337.43359 C 393.26477 344.10008 392.53436 338.21511 391.60547 349.89258 C 390.76411 360.46959 389.61491 365.91316 387.21289 370.68945 C 384.43998 376.20318 377.163 381.11007 375.67188 381.5 C 364.26821 386.35813 353.13946 389.55761 342.02539 391.20703 L 376.16406 256.39258 A 15.0015 15.0015 0 0 0 361.89453 237.7168 A 15.0015 15.0015 0 0 0 349.58398 243.76367 C 339.99305 256.67318 332.57022 265.64083 327.41016 270.42578 C 322.25009 275.21074 320.81817 274.94701 321.02539 274.95898 C 321.23262 274.97098 318.80453 274.45929 313.61133 269.43359 C 308.41813 264.4079 301.31229 255.8681 292.1582 244.72266 A 15.0015 15.0015 0 0 0 265.99219 257.78516 L 298.11328 389.95312 C 288.19205 388.02909 278.03066 385.05546 267.45898 381.07617 C 263.66132 376.22414 259.90522 375.63867 257.00781 370.07617 C 254.27151 364.67625 252.63392 357.66076 251.86719 348.05859 C 251.07202 338.10038 248.71045 338.8965 245.0625 330.90039 C 242.93884 326.24545 231.83166 305.32303 224.45508 292.08203 C 218.15682 280.77662 206.14335 255.78 203.72852 248.95508 C 197.95843 232.64725 195.65706 217.83217 196.21484 200.5957 C 197.94977 146.9828 234.16644 101.08752 286.76953 85.84375 C 297.57789 82.71161 309.59481 81.140854 321.51562 81.134766 z M 307.5293 301.66602 C 311.12511 303.36562 314.94418 304.65684 319.29492 304.9082 C 324.41671 305.20411 329.18441 304.02435 333.6582 302.03906 L 320.37109 354.50977 L 307.5293 301.66602 z M 403.0918 492.38477 C 350.62968 518.86909 297.64916 518.41661 241.04297 492.58203 C 244.17063 501.31318 257.19146 511.08545 242.70898 517.96289 C 224.12967 526.90162 222.07877 535.42687 244.37305 543.34375 C 257.11791 550.49808 261.80248 558.51777 246.03906 568.72656 C 236.40085 573.1632 220.16371 574.99462 247.70508 594.10742 C 250.15569 598.87641 270.28494 617.42929 273.19922 617.60547 C 274.92396 617.70974 289.07119 633.21314 290.17383 636.20703 C 291.45128 639.67559 294.94374 640.97978 304.5918 641.5957 C 326.59451 643.00032 348.00929 641.81001 351.10352 639.00977 C 352.19622 638.02089 353.0918 636.68845 353.0918 636.04883 C 353.0918 635.40922 356.46066 631.60633 360.58008 627.59766 C 364.69949 623.58899 369.31924 618.89499 370.8457 617.16602 C 372.37217 615.43705 374.96384 613.15531 376.60547 612.0957 C 381.84009 608.71692 403.0918 587.26963 403.0918 585.36523 C 391.96534 577.61689 394.88068 569.86748 403.0918 562.11914 C 429.29936 549.16937 404.95213 546.42967 403.0918 538.875 C 390.02575 529.67488 398.70371 522.88968 403.0918 515.62891 C 429.69246 502.56044 409.66831 498.81781 403.0918 492.38477 z "
+       transform="translate(174.73873,222.65557)"
+       id="path3854" />
+  </g>
+</svg>

icons/lightbulb-light.svg
@@ -1,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="642.19"
+   height="642.18774"
+   id="svg2"
+   version="1.1"
+   inkscape:version="0.92.1 r15371"
+   sodipodi:docname="lightbulb-light.svg"
+   inkscape:export-filename="/media/quentin/ESO/Firefox/addon-sdk/enlight/misc/lightbulb_off64.png"
+   inkscape:export-xdpi="8.9700003"
+   inkscape:export-ydpi="8.9700003">
+  <defs
+     id="defs4" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="0.65186683"
+     inkscape:cx="281.01508"
+     inkscape:cy="330.10043"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     inkscape:window-width="1572"
+     inkscape:window-height="871"
+     inkscape:window-x="0"
+     inkscape:window-y="0"
+     inkscape:window-maximized="1"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0"
+     inkscape:snap-midpoints="true"
+     inkscape:snap-page="true"
+     inkscape:snap-object-midpoints="true"
+     inkscape:snap-bbox="true"
+     inkscape:snap-bbox-midpoints="true"
+     inkscape:snap-nodes="true"
+     inkscape:snap-grids="false"
+     inkscape:snap-to-guides="false" />
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-174.73873,-222.65557)">
+    <path
+       style="fill:#bebebf;fill-opacity:0.94117647;stroke:none"
+       d="M 321.72656 0 C 303.80784 0.00915 285.74653 2.3681119 269.5 7.0761719 C 190.42995 29.989772 135.98871 98.976526 133.38086 179.56445 C 132.54244 205.47337 136.00249 227.74479 144.67578 252.25781 C 148.30561 262.51666 166.36289 300.08837 175.83008 317.08203 C 186.91815 336.98516 203.61448 368.43654 206.80664 375.43359 C 212.29003 387.4529 215.83991 394.69153 217.03516 409.66016 C 218.18768 424.0936 220.64866 434.64095 224.76172 442.75781 C 229.11694 451.11906 234.76422 451.99773 240.47266 459.29102 C 298.83602 481.2596 348.91157 483.02592 403.13086 459.92773 C 405.37225 459.3416 416.3124 451.96567 420.48047 443.67773 C 424.09107 436.49827 425.8193 428.31675 427.08398 412.41797 C 428.48023 394.86504 429.57813 395.27461 432.70312 385.25391 C 435.7007 375.64184 450.75374 344.28352 462.33789 323.52148 C 481.39165 289.37179 495.11576 260.64398 500.11328 244.44922 C 515.99979 192.96801 510.28573 139.29074 484.05078 93.552734 C 459.5028 50.755794 422.3163 21.755828 373.0957 7.0234375 C 357.42062 2.3316775 339.64529 -0.00915 321.72656 0 z M 321.51562 81.134766 C 333.43645 81.128677 345.26126 82.687299 355.68945 85.808594 C 388.43453 95.609644 413.17478 114.90145 429.50586 143.37305 C 446.95924 173.80127 450.76025 209.51271 440.19141 243.76172 C 436.8667 254.53564 427.73649 273.6464 415.06055 296.36523 C 407.35395 310.17763 397.33795 331.03895 395.34375 337.43359 C 393.26477 344.10008 392.53436 338.21511 391.60547 349.89258 C 390.76411 360.46959 389.61491 365.91316 387.21289 370.68945 C 384.43998 376.20318 377.163 381.11007 375.67188 381.5 C 364.26821 386.35813 353.13946 389.55761 342.02539 391.20703 L 376.16406 256.39258 A 15.0015 15.0015 0 0 0 361.89453 237.7168 A 15.0015 15.0015 0 0 0 349.58398 243.76367 C 339.99305 256.67318 332.57022 265.64083 327.41016 270.42578 C 322.25009 275.21074 320.81817 274.94701 321.02539 274.95898 C 321.23262 274.97098 318.80453 274.45929 313.61133 269.43359 C 308.41813 264.4079 301.31229 255.8681 292.1582 244.72266 A 15.0015 15.0015 0 0 0 265.99219 257.78516 L 298.11328 389.95312 C 288.19205 388.02909 278.03066 385.05546 267.45898 381.07617 C 263.66132 376.22414 259.90522 375.63867 257.00781 370.07617 C 254.27151 364.67625 252.63392 357.66076 251.86719 348.05859 C 251.07202 338.10038 248.71045 338.8965 245.0625 330.90039 C 242.93884 326.24545 231.83166 305.32303 224.45508 292.08203 C 218.15682 280.77662 206.14335 255.78 203.72852 248.95508 C 197.95843 232.64725 195.65706 217.83217 196.21484 200.5957 C 197.94977 146.9828 234.16644 101.08752 286.76953 85.84375 C 297.57789 82.71161 309.59481 81.140854 321.51562 81.134766 z M 307.5293 301.66602 C 311.12511 303.36562 314.94418 304.65684 319.29492 304.9082 C 324.41671 305.20411 329.18441 304.02435 333.6582 302.03906 L 320.37109 354.50977 L 307.5293 301.66602 z M 403.0918 492.38477 C 350.62968 518.86909 297.64916 518.41661 241.04297 492.58203 C 244.17063 501.31318 257.19146 511.08545 242.70898 517.96289 C 224.12967 526.90162 222.07877 535.42687 244.37305 543.34375 C 257.11791 550.49808 261.80248 558.51777 246.03906 568.72656 C 236.40085 573.1632 220.16371 574.99462 247.70508 594.10742 C 250.15569 598.87641 270.28494 617.42929 273.19922 617.60547 C 274.92396 617.70974 289.07119 633.21314 290.17383 636.20703 C 291.45128 639.67559 294.94374 640.97978 304.5918 641.5957 C 326.59451 643.00032 348.00929 641.81001 351.10352 639.00977 C 352.19622 638.02089 353.0918 636.68845 353.0918 636.04883 C 353.0918 635.40922 356.46066 631.60633 360.58008 627.59766 C 364.69949 623.58899 369.31924 618.89499 370.8457 617.16602 C 372.37217 615.43705 374.96384 613.15531 376.60547 612.0957 C 381.84009 608.71692 403.0918 587.26963 403.0918 585.36523 C 391.96534 577.61689 394.88068 569.86748 403.0918 562.11914 C 429.29936 549.16937 404.95213 546.42967 403.0918 538.875 C 390.02575 529.67488 398.70371 522.88968 403.0918 515.62891 C 429.69246 502.56044 409.66831 498.81781 403.0918 492.38477 z "
+       transform="translate(174.73873,222.65557)"
+       id="path3854" />
+  </g>
+</svg>

misc/web-ext_build_cmd.sh
@@ -1,0 +1,1 @@
+web-ext build -i env.sh -i misc/* --overwrite-dest

misc/web-ext_env.sh
@@ -1,0 +1,1 @@
+alias web-ext='node_modules/web-ext/bin/web-ext'

commit 4d32daa6924046366749b1c0e1898a90494b18d3

Fix a bug for auto-highlighting
Auto-highlighting was done on all pages, regardless of whether we had
detected raw text or not. It would break many pages. Fix the bug.

Fixes: 99821c9abdf9 ("Switch to web extension framework")

scripts/enlight-background.js
@@ -138,8 +138,9 @@ function checkBodyListener(p) {
     case "checkBodyPort":
       p.onMessage.addListener((m) => {
         console.debug("[enlight] Do we detect a code block?", m.isCodeBlock);
-        doHighlight("auto");
-        isHighlighted = true;
+        if (m.isCodeBlock)
+          doHighlight("auto");
+        isHighlighted = m.isCodeBlock;
       });
       break;
     case "detectedLanguage":