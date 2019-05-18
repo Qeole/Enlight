a997d7cb35dfa38682baf233b8ee6f69fdc05bb7
Qeole <qeole@outlook.com>
2019-05-19 00:25:26 +0100

    Add a preference to filter languages in popup and autodetection
    Using too many languages has two drawbacks.
    
    - It makes it much harder to find the desired languages in the popup.
      The one the user is looking for tends to be lost in the middle of a
      list of not-so-often used languages.
    - It considerably slows down autodetection.
    
    As an effort to use a subset of languages supported by the library,
    start adding a list of checkboxes for the different languages in the
    preference menu.
    
    This is the UI part. Options not saved or restored yet, and without
    effect.

---
+++ options/fill-lang-list.js
@@ -0,0 +1,52 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+var gLanguagePath  = "languages-list_all.json";
+
+/*
+ * Parse JSON list of languages.
+ */
+function loadJSON(aCallback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", gLanguagePath, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            aCallback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+/*
+ * Create list of languages.
+ */
+function updateList(aResponse) {
+    languageList = JSON.parse(aResponse);
+    let optList = document.getElementById("langlist");
+    for (let l in languageList) {
+        if (l == "auto")
+            continue;
+        let row = document.createElement("tr");
+        let cell = document.createElement("td");
+        let input = document.createElement("input");
+        let label = document.createElement("label");
+        let id = "langlist-" + l;
+        input.type = "checkbox";
+        input.id = id;
+        label.for = id;
+        label.textContent = languageList[l];
+        cell.appendChild(input)
+        cell.appendChild(label)
+        row.appendChild(cell);
+        optList.appendChild(row);
+    }
+}
+
+loadJSON(updateList);
