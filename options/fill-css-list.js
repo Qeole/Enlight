37f21a7a2bbe3c557029b683bc1a00e43af500ca
Qeole <qeole@outlook.com>
2021-06-14 01:09:40 +0100

    Update file headers: remove modelines, switch to SPDX tags
    Remove Vim and emacs modelines from the source files, they're not really
    in a fashion anymore and there are other ways to enforce format
    consistency.
    
    Remove licence headers from the source files: instead, use the more
    concise SPDX tags referring to the same licence. More information at
    https://spdx.org/.

--- options/fill-css-list.js
+++ options/fill-css-list.js
@@ -1,11 +1,4 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
-/* vim: set ts=8 sts=4 et sw=4 tw=80: */
-
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+/* SPDX-License-Identifier: MPL-2.0 */
 
 const styles = [
     { "file": "base16/3024.css",                         "name": "3024" },
--- options/shortcuts-lang-list.js
+++ options/shortcuts-lang-list.js
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
 function checkAll(aVal) {
     let table = document.getElementById("langlist");
 
